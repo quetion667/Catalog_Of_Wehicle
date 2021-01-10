3b9ca5cd0290a3830450ab35f10e6a3863f211dd
quetion667 <55294507+quetion667@users.noreply.github.com>
2021-01-10 20:38:31 +0500

    Add files via upload

---
+++ Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CatalogOfWehicle.Data;
+using CatalogOfWehicle.Models;
+
+namespace CatalogOfWehicle.Pages.Wehicles
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly CatalogOfWehicle.Data.CatalogOfWehicleContext _context;
+
+        public DeleteModel(CatalogOfWehicle.Data.CatalogOfWehicleContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Wehicle Wehicle { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Wehicle = await _context.Wehicle.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Wehicle == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Wehicle = await _context.Wehicle.FindAsync(id);
+
+            if (Wehicle != null)
+            {
+                _context.Wehicle.Remove(Wehicle);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
